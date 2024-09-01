cacadf30909dbc136a6ff91d729879ce984b7c3d
David Alcalde <davidtheindestructible@gmail.com>
2024-09-01 18:36:43 -0400

    Implement editing item rating and priority

---
+++ lib/widgets/utils/hover_builder.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+
+class HoverBuilder extends StatefulWidget {
+
+  final WidgetBuilder defaultBuilder;
+  final WidgetBuilder hoveredBuilder;
+
+  // AnimatedSwitcher params
+  final Duration duration;
+  final Duration? reverseDuration;
+  final Curve switchInCurve;
+  final Curve switchOutCurve;
+  final AnimatedSwitcherTransitionBuilder transitionBuilder;
+  final AnimatedSwitcherLayoutBuilder layoutBuilder;
+
+  const HoverBuilder({
+    required this.defaultBuilder,
+    required this.hoveredBuilder,
+    this.duration = const Duration(milliseconds: 150),
+    this.reverseDuration,
+    this.switchInCurve = Curves.easeOutCubic,
+    this.switchOutCurve = Curves.easeInCubic,
+    this.transitionBuilder = AnimatedSwitcher.defaultTransitionBuilder,
+    this.layoutBuilder = AnimatedSwitcher.defaultLayoutBuilder,
+    super.key,
+  });
+
+  @override
+  State<HoverBuilder> createState() => _HoverBuilderState();
+
+}
+
+class _HoverBuilderState extends State<HoverBuilder> {
+
+  bool isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      hitTestBehavior: HitTestBehavior.translucent,
+      onEnter: (event) {
+        if (isHovered) return;
+        setState(() {
+          isHovered = true;
+        });
+      },
+      onExit: (event) {
+        if (!isHovered) return;
+        setState(() {
+          isHovered = false;
+        });
+      },
+      child: AnimatedSwitcher(
+        duration: widget.duration,
+        reverseDuration: widget.reverseDuration,
+        switchInCurve: widget.switchInCurve,
+        switchOutCurve: widget.switchOutCurve,
+        transitionBuilder: widget.transitionBuilder,
+        layoutBuilder: widget.layoutBuilder,
+        child: isHovered
+            ? widget.hoveredBuilder(context)
+            : widget.defaultBuilder(context),
+      ),
+    );
+  }
+
+}
