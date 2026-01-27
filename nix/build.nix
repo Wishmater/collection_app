22990f1b0d484fb757a861d67344b350374f21c2
David Alcalde <david.alcalde.cu@gmail.com>
2026-01-27 16:35:35 -0500

    chore: update flutter framework and dependencies versions and add nix support

---
+++ nix/build.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> }:
+
+pkgs.flutter.buildFlutterApplication rec {
+
+  pname = "collection_app";
+  version = "0.0.1";
+
+  src = ./..;
+
+  # autoPubspecLock = src + "/pubspec.lock";
+  pubspecLock = pkgs.lib.importJSON (src + "/pubspec.lock.json");
+  gitHashes = pkgs.lib.importJSON (src + "/pubspecGitHashes.json");
+
+  nativeBuildInputs = with pkgs;
+    [
+
+    ];
+
+}
